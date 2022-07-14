f077815df2f9d916b7cdaa0fe91a0b0f8caad4dc
baudson <guibau2303@gmail.com>
2022-07-14 16:43:02 +0200

    exjs1-2

---
+++ ex1-2.js
@@ -0,0 +1,6 @@
+console.log("Exercice 1-2 js BeCode");
+
+let name1 = 'Lambert';
+let firstName = 'Gerard';
+let city = 'Paris';
+console.log(`Your name is ${firstName} ${name1} and you live in ${city}`);
