f174534642975240dd6199aee3a076393edab285
baudson <guibau2303@gmail.com>
2022-07-18 12:25:22 +0200

    finished 3 and 4

---
+++ ex3-1.js
@@ -0,0 +1,8 @@
+
+
+let arr= [1, 2, 3, 4, 5];
+let sum=0;
+for (const i of arr) {
+    sum+=i;
+}
+console.log(sum);
---
+++ ex3-3.js
@@ -0,0 +1,11 @@
+
+
+let arr = [1, 2, 3, 4, 5];
+let arrCopy = [];
+for(let i of arr)
+{
+    arrCopy.push(i);
+}
+console.log(arrCopy);
+let arrCopy2 = [...arr];
+console.log(arrCopy2);
